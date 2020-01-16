bfbb0bcfe222c2e72e56dd98d599424fca285557
andrew stehno <andrew_stehno@yahoo.com>
2020-01-15 22:44:31 -0700

    added proxy in package.json, completed books api call.

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes/apiRoutes");
+
+// Define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Add routes, both API and view
+app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
